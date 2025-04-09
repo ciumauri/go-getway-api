62a0210825743256567828ea086ec4be2900a54d
Mauricio Oliveira <ciumauri@gmail.com>
2025-04-08 22:41:16 -0300

    feat: Criando interface invoiceRepository

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -6,3 +6,10 @@ type AccountRepository interface {
 	GetByID(id string) (*Account, error)
 	UpdateBalance(account *Account) error
 }
+
+type InvoiceRepository interface {
+	CreateInvoice(invoice *Invoice) error
+	GetByID(id string) (*Invoice, error)
+	GetByAccountID(accountID string) ([]*Invoice, error)
+	UpdateStatus(invoice *Invoice) error
+}
