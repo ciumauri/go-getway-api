0a09b7765a77021911c0c0072787c540cb73f2fb
Mauricio Oliveira <ciumauri@gmail.com>
2025-04-08 22:41:29 -0300

    feat: Adicionando erros de invoice

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -5,12 +5,15 @@ import "errors"
 var (
 	// ErrAccountNotFound is returned when an account is not found
 	ErrAccountNotFound = errors.New("account not found")
-	// ErrDuplicateAccount is returned when a duplicate account is found
-	ErrDuplicateAccount = errors.New("account already exists")
 	// ErrDuplicatedAPIKey is returned when a duplicated API key is found
 	ErrDuplicatedAPIKey = errors.New("API key already exists")
 	// ErrInvoiceNotFound is returned when an invoice is not found
 	ErrInvoiceNotFound = errors.New("invoice not found")
 	// ErrUnauthorizedAccess is returned when a user is not authorized to access a resource
 	ErrUnauthorizedAccess = errors.New("unauthorized access")
+
+	// ErrInvalidAmount is returned when an invalid amount is provided
+	ErrInvalidAmount = errors.New("invalid amount")
+	// ErrInvalidStatus is returned when an invalid status is provided
+	ErrInvalidStatus = errors.New("invalid status")
 )
